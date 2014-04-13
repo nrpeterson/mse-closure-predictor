868cba3d489b157f52c6078d96cad80ef1a740e8
Nicholas R. Peterson <nrpeterson@gmail.com>
2014-04-12 21:05:39 -0400

    Renamed some files; finished extracting metrics to CSV

--- cleandata.py
+++ cleandata.py
@@ -1,29 +1,28 @@
 import json
-import pprint
 import csv
-pp = pprint.PrettyPrinter()
 
-f = open('rawdata.json', 'r')
-
-rawdata = json.load(f)
-data = list()
-
-demands = ['prove', 'Prove', 'show', 'Show', 'compute', 'Compute', 'calculate', 'Calculate', 'find', 'Find', 'Explain', 'explain']
-
-txtspeak = [' i ', ' u ']
-
-pleas = ['help', 'Help', "don't understand", "don't get it", "don't see how", 'show me', 'Show me', 'stuck', 'Stuck']
-
-polite = ['please', 'Please', 'thanks', 'Thanks', 'Thank you', 'thank you']
-
-effort = ['I tried', "I've tried", "My attempt", 'my attempt', 'work so far']
-
-questions = ['where', 'Where', 'what', 'What', 'when', 'When', 'why', 'Why', 'how', 'How', 'who', 'Who']
-
+# List of fields that will occur in the CSV file of cleaned date, so that
+# this list can be imported by other code rather than inferred.
 fields = ['id', 'author_rep', 'calculus', 'closed', 'colons', 'commands', 'commas', 'dollars', 'doubledollars', 'effort', 'homework', 'num_tags', 'paragraphs', 'periods', 'pleas', 'politeness', 'post_length', 'precalc', 'questionmarks', 'questions', 'quotes', 'spaces', 'title_length', 'txtspeak']
 
 
-for item in rawdata:
+def extract_data(item):
+    """Given item (a dict extracted from StackExchange JSON data), extract 
+    metrics and return a dict of the results.
+    """
+    
+    # Define some word groups to search for as part of data measurement.
+    demands = ['prove', 'Prove', 'show', 'Show', 'compute', 'Compute', 
+            'calculate', 'Calculate', 'find', 'Find', 'Explain', 'explain']
+    txtspeak = [' i ', ' u ']
+    pleas = ['help', 'Help', "don't understand", "don't get it", 
+            "don't see how", 'show me', 'Show me', 'stuck', 'Stuck']
+    polite = ['please', 'Please', 'thanks', 'Thanks', 'Thank you', 'thank you']
+    effort = ['I tried', "I've tried", "My attempt", 'my attempt', 
+            'work so far']
+    questions = ['where', 'Where', 'what', 'What', 'when', 'When', 'why', 
+            'Why', 'how', 'How', 'who', 'Who']
+
     stats = dict()
     
     stats['id'] = item['question_id']
@@ -34,10 +33,13 @@ for item in rawdata:
         stats['author_rep'] = 1
     
     if 'closed_details' in item:
-        stats['closed'] = int('context' in item['closed_details']['description'])
+        desc = item['closed_details']['description']
+        stats['closed'] = int('context' in desc)
     else:
         stats['closed'] = 0
-    stats['calculus'] = int('calculus' in item['tags'] or 'multivariable-calculus' in item['tags'])
+    
+    stats['calculus'] = int('calculus' in item['tags'] or 
+            'multivariable-calculus' in item['tags'])
     stats['colons'] = item['body'].count(':')
     stats['commands'] = sum([item['body'].count(word) for word in demands])
     stats['commas'] = item['body'].count(',')
@@ -59,7 +61,18 @@ for item in rawdata:
     stats['title_length'] = len(item['title'])
     stats['txtspeak'] = sum([item['body'].count(word) for word in txtspeak])
 
-    data.append(stats)
-    if stats['effort'] > 0 and stats['closed']:
-        pp.pprint(stats)
+    return stats
+
+# If the script is called directly, process the file 'rawdata.json' to extract
+# metric information in to cleandata.csv
+if __name__ == "__main__":
+    rawdatafile = open('rawdata.json', 'r')
+    rawdata = json.load(rawdatafile)
+    
+    cleandatafile = open('cleandata.csv', 'w')
+    writer = csv.DictWriter(cleandatafile, fields)
+    writer.writeheader()
+
+    for item in rawdata:
+        writer.writerow(extract_data(item))
 
